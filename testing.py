36d6859ea9e5e91a5a351ca6a1dd9dbdb28d6af2
foryourselfand <foryourselfand@gmail.com>
2019-11-03 14:01:21 +0300

    simple example

--- testing.py
+++ testing.py
@@ -1,1 +1,8 @@
-print('check 1')
+import json
+from pprint import pprint
+
+with open('p3111_timetable.json', 'r', encoding='utf-8') as file:
+    input_data = file.read()
+    data = json.loads(input_data)
+
+pprint(data)
